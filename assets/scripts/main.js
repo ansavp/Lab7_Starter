704eccfe4c513ca2c5418468f1c3951cdcb05562
ansavp <akpanda@ucsd.edu>
2021-11-07 16:24:10 -0800

    update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -100,26 +100,34 @@ async function fetchRecipes() {
  * appends them to the page
  */
 function createRecipeCards() {
-  // Makes a new recipe card
-  const recipeCard = document.createElement('recipe-card');
+  for (let i = 0; i < Object.keys(recipeData).length; ++i) {
+    // Makes a new recipe card
+    const recipeCard = document.createElement('recipe-card');
+
+    if (i > 2) {
+      recipeCard.classList.add("hidden");
+    }
+
   // Inputs the data for the card. This is just the first recipe in the recipes array,
   // being used as the key for the recipeData object
-  recipeCard.data = recipeData[recipes[0]];
+  recipeCard.data = recipeData[recipes[i]];
 
   // This gets the page name of each of the arrays - which is basically
   // just the filename minus the .json. Since this is the first element
   // in our recipes array, the ghostCookies URL, we will receive the .json
   // for that ghostCookies URL since it's a key in the recipeData object, and
   // then we'll grab the 'page-name' from it - in this case it will be 'ghostCookies'
-  const page = recipeData[recipes[0]]['page-name'];
+  const page = recipeData[recipes[i]]['page-name'];
   router.addPage(page, function() {
     document.querySelector('.section--recipe-cards').classList.remove('shown');
     document.querySelector('.section--recipe-expand').classList.add('shown');
-    document.querySelector('recipe-expand').data = recipeData[recipes[0]];
+    document.querySelector('recipe-expand').data = recipeData[recipes[i]];
   });
   bindRecipeCard(recipeCard, page);
 
   document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard);
+}
+
 
   /**
    * TODO - Part 1 - Step 3
@@ -131,6 +139,7 @@ function createRecipeCards() {
    * After this step you should see multiple cards rendered like the end of the last
    * lab
    */
+
 }
 
 /**
